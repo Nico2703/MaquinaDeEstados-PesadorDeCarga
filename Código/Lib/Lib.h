6eaecdedd867bf60666850fd1772437cb6634d19
NicoF2703 <NicoF2703@users.noreply.github.com>
2020-09-04 18:34:40 -0300

    Agregado

--- Código/Lib/Lib.h
+++ Código/Lib/Lib.h
@@ -3,9 +3,9 @@
 #include <stdio.h>
 
 typedef enum {
-	espera = 1;
-    avisomaximo = 0;
-    avisosobrecarga = 0;
+	espera = 1,
+    avisomaximo = 0,
+    avisosobrecarga = 0,
 }estados_c;
 
 typedef struct {
@@ -14,7 +14,7 @@ typedef struct {
 	char tolerancia;    // Delta de carga
 }carga_c;
 
-carga_c f_inicio(void); // lee el archivo de configuración y carga las variables.
+carga_c f_inicio(void); // Lee el archivo de configuración y carga las variables.
 estados_c f_espera(carga_c);
 estados_c f_cargamax(carga_c);
 estados_c f_sobrecarga(carga_c);
