25302d05a8694879cd2f1a5587c00d1b919d903d
NicoF2703 <NicoF2703@users.noreply.github.com>
2020-09-06 10:55:46 -0300

    Agregado

--- Código/Src/Main.cpp
+++ Código/Src/Main.cpp
@@ -10,14 +10,20 @@ int main() {
     carga_c config;
     estados_c estado = espera; 
     
-    config = f_inicio();
+    //config = f_inicio();
     while (1) {
         switch (estado) {
-        case espera: estado = f_espera(config);
+        case espera: 
+        config = f_inicio();
+		estado = f_espera(config);
             break;
-        case avisomaximo: estado = f_cargamax(config);
+        case avisomaximo: 
+        config = f_inicio();
+		estado = f_cargamax(config);
             break;
-        case avisosobrecarga: estado = f_sobrecarga(config);
+        case avisosobrecarga: 
+        config = f_inicio();
+		estado = f_sobrecarga(config);
             break;
         }
     }
@@ -26,9 +32,9 @@ int main() {
 
 carga_c f_inicio(void)
     {
+    system ("CLS");
     carga_c aux;
     string comentarios;
-    //aux.carga=0;
 	archivo.open("Config.txt",ios::in);
 	if (archivo.is_open())
 	{	
@@ -55,6 +61,8 @@ estados_c f_espera(carga_c config)
     {
 	  printf("Sistema en espera\n\n");
       system("PAUSE");
+      estado = espera;
+      return (estado);
     }
     estado = avisomaximo;
     return (estado);
@@ -67,6 +75,8 @@ estados_c f_cargamax(carga_c config)
     {
         printf("Carga completa\n\n");
         system ("PAUSE");
+        estado = avisomaximo;
+        return (estado);
     }
     if (config.carga < config.carga_max)
     {
@@ -87,7 +97,10 @@ estados_c f_sobrecarga(carga_c config)
     {
         printf("Sobrecarga\n\n");
         system ("PAUSE");
+        estado = avisosobrecarga;
+        return (estado);
     }
     estado = avisomaximo;
     return (estado);
 }
+
